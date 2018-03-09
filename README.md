fc4f5078a356fd648e03512e5d619dde3f9b3334
mapetric <32669888+mapetric@users.noreply.github.com>
2018-03-09 18:49:27 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-frontend-nanodegree-arcade-game
-===============================
+# Classic arcade game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Frogger inspired classic arcade game
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+### How to play
+
+* Open https://mapetric.github.io/arcade-game. This is the live demo.
+* You move by using arrow keys.
+* Your goal is to collect as many gems as you can and deposit them into the river.
+* Be careful if you get hit by a bug you are going to lose three gems you are carrying.
+* Every time you deposit five or more gems at once you get an additional life.
+* Every forth gem you pick up another enemy is going to spawn.
+* If you die and there are more than three enemies present one of them will despawn.
+* You can change the basic setting in settings.js.
+* You can change your character by using numbers (1-5).
+
+### Screenshots
+
+![Game screenshot](http://i68.tinypic.com/29bmr94.jpg)
+
+
+### External libraries Used
+
+* jQuery
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let lives = 5;
 let gemsCarried = 0;
 let gemsDeposited = 0;
-let enemies = 5;
+let enemies = 3;
 let gemsPickedUp = 0;
 let listening = true;
+let lives = startingLives;
 
 const $lives = $('#lives').children().first();
 const $gemsCarried = $('#gemsCarried').children().first();
@@ -11,90 +11,37 @@ const $gemsDeposited = $('#gemsDeposited').children().first();
 const $enemies = $('#enemies').children().first();
 const $modal = $('#myModal');
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// object that holds info about enemies
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+const Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// function that multiplies the speed by dt and moves the bug to the left if it leaves the screen
+// it also calls checkCollision
+
 Enemy.prototype.update = function(dt) {
+
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 
     this.x = this.x + dt * this.speed;
     if (this.x > 520) {
-          this.x = -100;
+          this.x = -120;
    }
    this.checkCollision();
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-var Gem = function(x, y, z) {
-      this.sprite = ['images/gem-blue.png', 'images/gem-green.png', 'images/gem-orange.png'][z];
-      this.value = [1, 2, 3][z];
-      this.x = x;
-      this.y = y;
-};
-
-function getRandomGem() {
-      let gemIndex = Math.floor(Math.random() * 100  + 1);
-      if (gemIndex > 95) {
-            gemIndex = 2;
-      } else if (gemIndex > 80) {
-            gemIndex = 1;
-      } else {
-            gemIndex = 0;
-      }
-      return gemIndex;
-}
-
-Gem.prototype.render = function() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Gem.prototype.checkPickup = function() {
-      if (player.y + 131 >= this.y + 90 && player.x + 25 <= this.x + 88 && player.y + 73 <= this.y + 135 && player.x + 76 >= this.x + 11) {
-            console.log('pickup');
-            gemsPickedUp++;
-            gemsCarried += gem.value;
-            $gemsCarried.text(gemsCarried);
-            gem = new Gem(101 * Math.floor(Math.random() * 5), 400 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
-            if (gemsPickedUp % 3 === 0){
-                  allEnemies.push(new Enemy(0, Math.random() * 184 + 50, Math.random() * 300));
-                  enemies++;
-                  $enemies.text(enemies);
-            }
-      }
-}
-
-var Player = function(x, y) {
-      this.sprite = 'images/char-boy.png';
-      this.x = x;
-      this.y = y;
-};
-
+// function that checks if the player crashed into an enemy, reduces the number of lives and displays it
+// if you lose all your lives it calls end function, if the number of enemies is bigger than startingEnemies remove one and display it
+// decrease the number of gems carried by lostGems and set them to 0 if you have less than lostGems also display it
 
 Enemy.prototype.checkCollision = function() {
       if (player.y + 131 >= this.y + 90 && player.x + 25 <= this.x + 88 && player.y + 73 <= this.y + 135 && player.x + 76 >= this.x + 11) {
-            console.log('collided');
             player.x = 202;
             player.y = 400;
             lives--;
@@ -102,7 +49,12 @@ Enemy.prototype.checkCollision = function() {
             if (lives === 0) {
                   end()
             }
-            gemsCarried -= 3;
+            if (enemies > startingEnemies) {
+                  enemies--;
+                  allEnemies.pop();
+                  $enemies.text(enemies);
+            }
+            gemsCarried -= lostGems;
             if (gemsCarried < 0) {
                   gemsCarried = 0;
             }
@@ -110,7 +62,73 @@ Enemy.prototype.checkCollision = function() {
       }
 };
 
+// Draw the enemy on the screen, required method for game
 
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// object that holds info about gems
+
+const Gem = function(x, y, gemIndex) {
+      this.sprite = ['images/gem-blue.png', 'images/gem-green.png', 'images/gem-orange.png'][gemIndex];
+      this.value = [1, 2, 3][gemIndex];
+      this.x = x;
+      this.y = y;
+};
+
+// function that gets a random index for our Gem object ()
+// the index depends on the perchantage set by orangeGemChance and greenGem chance;
+
+function getRandomGem() {
+      let gemIndex = Math.floor(Math.random() * 100  + 1);
+      if (gemIndex > orangeGemChance) {
+            gemIndex = 2;
+      } else if (gemIndex > greenGemChance) {
+            gemIndex = 1;
+      } else {
+            gemIndex = 0;
+      }
+      return gemIndex;
+}
+
+// Draw the gem on the screen, required method for game
+
+Gem.prototype.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// function that checks if the player picked up a gem, it increments gemsPickedUp, and adds the value of the picked up gem to the gemsCarried
+// it updates the display with gemsCarried. A new gem is created to replace the last one and every enemyEveryNGems a new enemy is spawned
+// enemies is incremented and the display is updated
+
+Gem.prototype.checkPickup = function() {
+      if (player.y <= this.y && player.x + 25 <= this.x + 88 && player.y + 60 >= this.y && player.x + 76 >= this.x + 11) {
+            console.log('pickup');
+            gemsPickedUp++;
+            gemsCarried += gem.value;
+            $gemsCarried.text(gemsCarried);
+            gem = new Gem(101 * Math.floor(Math.random() * 5) + 25, 460 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
+            if (gemsPickedUp % enemyEveryNGems === 0){
+                  allEnemies.push(new Enemy(0, Math.random() * 184 + 50, Math.random() * 200 + 100));
+                  enemies++;
+                  $enemies.text(enemies);
+            }
+      }
+}
+
+// object that holds info about our player
+
+const Player = function(x, y) {
+      this.sprite = 'images/char-boy.png';
+      this.x = x;
+      this.y = y;
+};
+
+// function that limits the player from leaving the canvas and checks if the player got to the river
+// if the player got to the river increase gemsDeposited by gemsCarried if you had 5 or more get a life
+// set gems carried to 0 and display everything. Check if the player picked up a gem.
 
 Player.prototype.update = function() {
       if (this.x > 400) {
@@ -129,7 +147,7 @@ Player.prototype.update = function() {
             player.x = 202;
             player.y = 400;
             gemsDeposited += gemsCarried;
-            if (gemsCarried > 5) {
+            if (gemsCarried >= 5) {
                   lives++;
             }
             gemsCarried = 0;
@@ -140,9 +158,14 @@ Player.prototype.update = function() {
       gem.checkPickup();
 };
 
+// Draw the player on the screen, required method for game
+
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// function that moves the player accoriding to the pressed key.
+// it also sets the player image according to the pressed key.
 
 Player.prototype.handleInput = function (keyPressed) {
       switch (keyPressed) {
@@ -157,15 +180,35 @@ Player.prototype.handleInput = function (keyPressed) {
                   break;
             case 'down':
                   this.y += 83;
+                  break;
+            case 'char1':
+                  this.sprite = 'images/char-boy.png';
+                  break;
+            case 'char2':
+                  this.sprite = 'images/char-cat-girl.png';
+                  break;
+            case 'char3':
+                  this.sprite = 'images/char-horn-girl.png';
+                  break;
+            case 'char4':
+                  this.sprite = 'images/char-pink-girl.png';
+                  break;
+            case 'char5':
+                  this.sprite = 'images/char-princess-girl.png';
       }
 }
+
+// restart function that sets listening to true so we can track key presses
+// close the modal and clear the allEnemies array. Set lives and enemies gemsCarried, gemsDeposited and gemsPickedUp to initial values
+// display it create a new player at the starting position and create a new gem.
+// Create startingEnemies number of bugs.
 
 function restart() {
       listening = true;
       $modal.css('display', 'none');
       allEnemies = [];
-      lives = 5;
-      enemies = 5;
+      lives = startingLives;
+      enemies = startingEnemies;
       gemsCarried = 0;
       gemsDeposited = 0;
       gemsPickedUp = 0;
@@ -173,12 +216,15 @@ function restart() {
       $gemsCarried.text(gemsCarried);
       $gemsDeposited.text(gemsDeposited);
       $lives.text(lives);
-      var player = new Player(202, 400);
-      let gem = new Gem(101 * Math.floor(Math.random() * 5), 400 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
-      for (let i = 0; i < 5; i++) {
+      player = new Player(202, 400);
+      gem = new Gem(101 * Math.floor(Math.random() * 5) + 25 , 460 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
+      for (let i = 0; i < startingEnemies; i++) {
             allEnemies.push(new Enemy(0, Math.random() * 184 + 50, Math.random() * 200 + 100));
       }
 }
+
+// function that calls populateModal, disables key press tracking, display the Modal
+// add an event listener that closes the modal and calls restart
 
 function end() {
       populateModal();
@@ -189,6 +235,8 @@ function end() {
       });
 }
 
+// function that updates the modal with the number of gemsDeposited and the number of enemies present at the end.
+
 function populateModal() {
       $('#modal-text').children().first().text(gemsDeposited);
       $('#modal-text').children().next().text(enemies);
@@ -196,22 +244,38 @@ function populateModal() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
 let allEnemies = [];
-var player = new Player(202, 400);
-let gem = new Gem(101 * Math.floor(Math.random() * 5), 400 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
-for (let i = 0; i < 5; i++) {
+let player = new Player(202, 400);
+let gem = new Gem(101 * Math.floor(Math.random() * 5) + 25 , 460 - 83 * (Math.floor(Math.random() * 3) + 2) , getRandomGem());
+
+// create a startingEnemies number of enemies
+
+for (let i = 0; i < startingEnemies; i++) {
       allEnemies.push(new Enemy(0, Math.random() * 184 + 50, Math.random() * 200 + 100));
 }
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+// On (1-5) sends data for character image
+
 document.addEventListener('keyup', function(e) {
       const allowedKeys = {
           37: 'left',
           38: 'up',
           39: 'right',
-          40: 'down'
+          40: 'down',
+          49: 'char1',
+          50: 'char2',
+          51: 'char3',
+          52: 'char4',
+          53: 'char5',
+          97: 'char1',
+          98: 'char2',
+          99: 'char3',
+          100: 'char4',
+          101: 'char5'
       };
       if (listening) {
             player.handleInput(allowedKeys[e.keyCode]);
